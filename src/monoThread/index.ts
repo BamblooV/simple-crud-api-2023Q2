1db7b7d1a5db011e7880e97a143b8d5cfbcffb1c
Egor Gavrilenko <bambloo769@gmail.com>
2023-07-03 22:01:20 +0300

    refactor: change get routes routine

--- src/monoThread/index.ts
+++ src/monoThread/index.ts
@@ -1,5 +1,6 @@
 import Server from "../framework/Server";
-import getRouter from "./routes";
+import UserService from "../services/User.service";
+import getRouter from "../routes/routes";
 
 import dotenv from "dotenv";
 
@@ -9,6 +10,6 @@ const PORT = parseInt(process.env.PORT ?? "8000");
 
 const app = new Server();
 
-app.use(getRouter()).listen(PORT, () => {
+app.use(getRouter(new UserService())).listen(PORT, () => {
   console.log(`server started on ${PORT}`);
 });
